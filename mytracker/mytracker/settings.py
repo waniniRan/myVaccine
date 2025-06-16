0d1a962e224148f684c18b67d6444628e6549a85
Cindy Gichuhi <166112447+waniniRan@users.noreply.github.com>
2025-06-16 20:59:08 +0300

    Dash css

--- mytracker/mytracker/settings.py
+++ mytracker/mytracker/settings.py
@@ -132,3 +132,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/dashboard/'
 LOGOUT_REDIRECT_URL = '/login/'
 LOGIN_URL = '/login/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
