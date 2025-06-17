d272d450c159a6bffa9db79062126b28cd5f7bb2
Cindy Gichuhi <166112447+waniniRan@users.noreply.github.com>
2025-06-17 10:07:22 +0300

    Add css

--- mytracker/mytracker/settings.py
+++ mytracker/mytracker/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
@@ -132,4 +133,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/dashboard/'
 LOGOUT_REDIRECT_URL = '/login/'
 LOGIN_URL = '/login/'
-STATIC_URL = '/static/'
+
